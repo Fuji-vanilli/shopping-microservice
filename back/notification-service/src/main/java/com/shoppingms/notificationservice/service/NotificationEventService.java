b9e19786463140c46d92a4d1c7f7de62be9995d3
Fuji vanilli <ijufillivan@gmail.com>
2023-09-04 14:42:55 +0000

    update

--- back/notification-service/src/main/java/com/shoppingms/notificationservice/service/NotificationEventService.java
+++ back/notification-service/src/main/java/com/shoppingms/notificationservice/service/NotificationEventService.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Consumer;
-
 @Service
 public class NotificationEventService {
 
